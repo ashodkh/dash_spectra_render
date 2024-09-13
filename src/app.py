59ab359775293f699da7f0b6ebd2b5029df3e1fe
ashodkh <ask126@pitt.edu>
2024-09-12 22:19:12 -0400

    fixed naming

--- src/app.py
+++ src/app.py
@@ -1,46 +1,50 @@
 import numpy as np
+import pylab as plt
+import pickle
+plt.rcParams.update({'font.size': 26})
+from sklearn.preprocessing import StandardScaler
+import matplotlib.gridspec as gridspec
+import umap
+from matplotlib.colors import LinearSegmentedColormap, ListedColormap
+import matplotlib
 import dash_script
+from dash import html, dcc, Input, Output, Dash
+import plotly.express as px
+import pandas as pd
+import plotly.graph_objects as go
 
-data = np.load('spec4000.npz')
-wavelength = np.load('wavelength.npz')['arr_0']
+lines = ["HBETA", "OIII_5007", "HALPHA", "NII_6584"]
+lines_names = [r'H$\beta$', r'[OIII] $\lambda$5007', r'H$\alpha$', r'[NII] $\lambda$6584']
+lines_waves = [4862.7, 5008.2, 6564.6, 6585.3]  
 
-spectra = data['spectra']
-n = spectra.shape[0]
+embeddings_x = np.load('./data_for_umap_dash/umap_x.npy')
+embeddings_y = np.load('./data_for_umap_dash/umap_y.npy')
+color_coding = np.load('./data_for_umap_dash/color_coding.npy')
+rest_frame_spectra = np.concatenate((np.load('./data_for_umap_dash/rest_frame_spectra1.npy'),np.load('./data_for_umap_dash/rest_frame_spectra2.npy')), axis=0)
+fluxes = np.load('./data_for_umap_dash/fluxes.npy')
+wavelength = np.load('./data_for_umap_dash/wavelength.npy')
+bin_centers = np.load('./data_for_umap_dash/bin_centers.npy')
+h_beta_ews = np.load('./data_for_umap_dash/h_beta_ews.npy')
+oiii_ews = np.load('./data_for_umap_dash/oiii_ews.npy')
+h_alpha_ews = np.load('./data_for_umap_dash/h_alpha_ews.npy')
+nii_ews = np.load('./data_for_umap_dash/nii_ews.npy')
 
-n2_ha = data['log_NII_Ha']
-o3_hb = data['log_OIII_Hb']
+l = 2
+bpt_lines = [0,1,2,3]
+zooms = np.zeros([fluxes.shape[0],len(bpt_lines)])
 
-lines_to_mask = ['OII_doublet', 'OIII (5007)', 'H_alpha']
-lines_waves = [3728.5, 5008.2, 6564.6]
-masking_windows = [10,7,8]
-masked_spectra = np.zeros_like(spectra)
-for i in range(n):
-    masked_spectra[i,:] = spectra[i,:]
-    for l in range(len(lines_to_mask)):
-        # np.searchsorted is used to find the indices of the masking window edges
-        i1, i2 = np.searchsorted(wavelength, (lines_waves[l]-masking_windows[l], lines_waves[l]+masking_windows[l]))
-        x0, x1 = wavelength[i1-1], wavelength[i2+1]
-        y0, y1 = spectra[i,i1-1], spectra[i,i2+1]
-        # the emission line region is replaces by linearly interpolating the underlying continuum
-        masked_spectra[i,i1:i2] = np.interp(wavelength[i1:i2], [x0,x1], [y0,y1])
-        # the below if statement is to prevent masking absoprtion lines
-        if np.average(masked_spectra[i,i1:i2]) > np.average(spectra[i,i1:i2]):
-                masked_spectra[i,i1:i2] = spectra[i,i1:i2]
+for i,ll in enumerate(bpt_lines):
+    zooms[:,i] = lines_waves[ll]#*(1+z[6])
 
-# This dictionary is used to tell the dash function which wavelengths to zoom in on and show in subplots titled by the keys.
-# The values must be a list/array of size len(x), so if you want to use the same value for each point, you can simply do [value]*number of points
-zoom = {}
-for l in range(len(lines_to_mask)):
-     zoom.update({lines_to_mask[l]: [lines_waves[l]]*n})
-
-app = dash_script.dash_plot_spectra(x={'n2_ha': n2_ha}, y={'o3_hb': o3_hb}, xlim=[-1.5,1], ylim=[-1.2,1], color_code={'redshift': data['z']}, cmap='Inferno', kao_lines=True, \
-                                    spectra=[spectra, masked_spectra], wavelength=wavelength, spec_colors=['rgba(0,0,0,0.5)','rgba(256,0,0,0.5)'],\
-                                    spec_names=['Observed', 'masked'],\
-                                    zoom=zoom, zoom_windows=[15,15,15],\
-                                    zoom_extras = [{'OII_doublet flux': spectra[:,np.searchsorted(wavelength, lines_waves[0])]},
-                                                   {'OIII(5007) flux': spectra[:,np.searchsorted(wavelength, lines_waves[1])-1]},
-                                                   {'H_alpha flux': spectra[:,np.searchsorted(wavelength, lines_waves[2])-1]}])
-
-server = app.server
+app = dash_script.dash_plot_spectra(x={'Continuum UMAP axis 1': embeddings_x}, y={'Continuum UMAP axis 2':embeddings_y},\
+                                    color_code={'H_alpha EW': np.clip(h_alpha_ews,a_min=0,a_max=20)},\
+                                    spectra=[rest_frame_spectra, fluxes], spec_colors=['rgba(0,256,100,0.5)', 'rgba(256,165,0,1)'],\
+                                    spec_names=['Observed Spectrum', 'Masked Continuum'], wavelength=[wavelength, bin_centers],\
+                                    kao_lines=False,\
+                                    zoom={'1':zooms[:,0],'2':zooms[:,1],'3':zooms[:,2],'4':zooms[:,3]}, zoom_windows=[15,15,15,15],\
+                                    zoom_extras = [{'H_beta EW': h_beta_ews},\
+                                                {'[OIII] EW': oiii_ews},\
+                                                {'H_alpha EW': h_alpha_ews},\
+                                                {'[NII] EW': nii_ews}])
 if __name__ == '__main__':
     app.run_server(debug=True)
